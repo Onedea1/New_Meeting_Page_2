cf06f1e65fe530b152786d6c25eaafcac33a650c
minhyeok <sun98034@gmail.com>
2023-05-24 12:03:20 +0900

    textfield need upgrade

--- lib/buttons/choice_resistar_button.dart
+++ lib/buttons/choice_resistar_button.dart
@@ -11,71 +11,122 @@ class ChoiceResisterButton extends StatefulWidget {
 class _ChoiceResisterButtonState extends State<ChoiceResisterButton> {
   bool _ischecked1 = false;
   bool _ischecked2 = false;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        SizedBox(
-          height: 48,
-          width: 165,
-          child: ElevatedButton(
-            child: Text(
-              '자유롭게 가입',
-              style: TextStyle(
-                  fontFamily: 'SUIT',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500),
-            ),
-            style: ElevatedButton.styleFrom(
-              elevation: 0,
-              backgroundColor: _ischecked1 ? MIXIN_ : MIXIN_WHITE,
-              foregroundColor: _ischecked1 ? MIXIN_2 : MIXIN_BLACK_4,
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: _ischecked1 ? MIXIN_2 : MIXIN_BLACK_4,
-                  width: 1.0,
+        Row(
+          children: [
+            SizedBox(
+              height: 48,
+              width: 165,
+              child: ElevatedButton(
+                child: Text(
+                  '자유롭게 가입',
+                  style: TextStyle(
+                      fontFamily: 'SUIT',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500),
                 ),
-                borderRadius: BorderRadius.circular(8),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _ischecked1 ? MIXIN_ : MIXIN_WHITE,
+                  foregroundColor: _ischecked1 ? MIXIN_2 : MIXIN_BLACK_4,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: _ischecked1 ? MIXIN_2 : MIXIN_BLACK_4,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    _ischecked1 = true;
+                    _ischecked2 = false;
+                  });
+                },
               ),
             ),
-            onPressed: () {setState(() {
-              _ischecked1 = true;
-              _ischecked2 = false;
-            });},
-          ),
-        ),
-        SizedBox(
-          width: 12,
-        ),
-        SizedBox(
-          height: 48,
-          width: 165,
-          child: ElevatedButton(
-            child: Text(
-              '승인 후 가입',
-              style: TextStyle(
-                  fontFamily: 'SUIT',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500),
+            SizedBox(
+              width: 12,
             ),
-            style: ElevatedButton.styleFrom(
-              elevation: 0,
-              backgroundColor: _ischecked2 ? MIXIN_ : MIXIN_WHITE,
-              foregroundColor: _ischecked2 ? MIXIN_2 : MIXIN_BLACK_4,
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: _ischecked2 ? MIXIN_2 : MIXIN_BLACK_4,
-                  width: 1.0,
+            SizedBox(
+              height: 48,
+              width: 165,
+              child: ElevatedButton(
+                child: Text(
+                  '승인 후 가입',
+                  style: TextStyle(
+                      fontFamily: 'SUIT',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500),
                 ),
-                borderRadius: BorderRadius.circular(8),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _ischecked2 ? MIXIN_ : MIXIN_WHITE,
+                  foregroundColor: _ischecked2 ? MIXIN_2 : MIXIN_BLACK_4,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: _ischecked2 ? MIXIN_2 : MIXIN_BLACK_4,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    _ischecked1 = false;
+                    _ischecked2 = true;
+                  });
+                },
               ),
             ),
-            onPressed: () {setState(() {
-              _ischecked1 = false;
-              _ischecked2 = true;
-            });},
-          ),
+          ],
         ),
+        if (_ischecked2)
+          Column(
+            children: [
+              SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'SUIT',
+                  fontWeight: FontWeight.w600,
+                ),
+                decoration: InputDecoration(
+                  hintText: '     가입 조건을 작성해주세요',
+                  hintStyle: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'SUIT',
+                    fontWeight: FontWeight.w500,
+                    color: MIXIN_BLACK_5,
+                  ),
+                  counterStyle: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'SUIT',
+                    color: Color(0xFFCED0D5),
+                  ),
+                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    borderSide: BorderSide(
+                      color: MIXIN_BLACK_4,
+                    ),
+                  ),
+                  // counterText: '',
+                  filled: true,
+                  fillColor: MIXIN_BLACK_4,
+                  contentPadding: EdgeInsets.symmetric(vertical: 10.0),
+                ),
+                maxLength: 80,
+              ),
+            ],
+          ),
       ],
     );
   }
